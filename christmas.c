bd12b104896917a97d997a7d8946f6c28a17bb65
dgamer10 <79217683+dgamer10yt@users.noreply.github.com>
2022-12-17 11:50:37 +0200

    Add files via upload

---
+++ christmas.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("Introduceti valoarea numarului natural N: ");
+    if(n % 7)
+    {
+        printf("Merry\n");
+    }
+    else
+    if(n % 11)
+    {
+        printf("Christmas\n");
+    }
+    else
+    if(n % 11; n % 7)
+    {
+        printf("MerryChristmas\n");
+    }
+    else
+    if((n % 11)== 0; (n % 7) == 0)
+    {
+        printf("%i", n);
+    }
+}
